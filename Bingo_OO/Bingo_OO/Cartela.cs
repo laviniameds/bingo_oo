9547154f28e4e1b9affb6293239be5fd7ceb6c0e
laviniameds <laviniameds@icloud.com>
2016-01-15 02:49:10 -0300

    9º

--- Bingo_OO/Bingo_OO/Cartela.cs
+++ Bingo_OO/Bingo_OO/Cartela.cs
@@ -13,24 +13,56 @@ namespace Bingo_OO
 {
     public partial class Cartela : Form 
     {
+        int contGanhou = 0;
         public Cartela()
         {
             InitializeComponent();
             classCartela c = new classCartela();
             Button[,] botoes = new Button[5,5];
             int[] array = c.encherCartela();
+            int cont = 0;
             for (int i = 0; i < 5; i++)
                 for (int j = 0; j < 5; j++)
                 {
                     if (!(i == 2 && j == 2))
                     {
                         botoes[i, j] = new Button();
-                        botoes[i, j].Text = array[i].ToString();
+                        botoes[i, j].Text = array[cont].ToString();
                         botoes[i, j].Size = new Size(62, 56);
                         botoes[i, j].Location = new Point(61 * i, 52 * j);
-                        panel1.Controls.Add(botoes[i,j]);
+                        botoes[i, j].Click += new EventHandler(this.botoes_Click);
+                        panel1.Controls.Add(botoes[i, j]);
+                        cont++;
                     }
+                }               
+        }
+
+        protected void botoes_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            classCartela c = new classCartela();
+            for (int i = 0; i < c.armSorteio.Length; i++)
+                if (btn.Text == c.armSorteio.GetValue(i).ToString())
+                {
+                    btn.Enabled = false;
+                    contGanhou++;
                 }
+            if (contGanhou == 24)
+            {
+                MessageBox.Show("Parabéns " + ActiveForm.Text + "! Você ganhou o jogo :D");
+                Form[] forms = Application.OpenForms.Cast<Form>().ToArray();
+                foreach (Form f in forms)
+                {
+                    if(f.Name == "Bingo")
+                    {
+                        f.MainMenuStrip.Items[0].Enabled = true;
+                        f.MainMenuStrip.Items[1].Enabled = false;
+                        f.MainMenuStrip.Items[2].Enabled = false;
+                        for (int i = 0; i < 75; i++) c.armSorteio.SetValue(0, i);
+                    }
+                    else f.Close();
+                }
+            }
         }
     }
 }
